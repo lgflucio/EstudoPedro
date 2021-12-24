da703992a82ee14cc00ed5627f3d2b65147a9841
pmslucio <pmsl12123@gmail.com>
2021-12-24 20:46:39 -0300

    Programa para verificar area da circunferencia

--- estudo1/Program.cs
+++ estudo1/Program.cs
@@ -10,7 +10,19 @@ namespace estudo1
     {
         static void Main(string[] args)
         {
+            double Area;
+            double n = Math.PI;
+            double Raio;
 
+
+            Console.WriteLine("Insira os Dados de entrada:");
+            Raio = Convert.ToDouble(Console.ReadLine());
+
+            Area = n * (Raio * Raio);
+
+            Console.WriteLine($"A área da cincunfêrencia é: {Area}");
+            Console.ReadKey();
         }
+         
     }
 }
